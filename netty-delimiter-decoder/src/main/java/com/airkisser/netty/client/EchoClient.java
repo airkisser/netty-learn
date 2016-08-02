3bb6fd1e4a175887e02e55568d46fb08b6635847
airkisser <luo1jun1@126.com>
2016-08-03 00:57:54 +0800

    netty DelimiterBasedFrameDecoder分隔符解码器使用 netty的DelimiterBasedFrameDecoder分隔符解码器使用，实现客户端向服务端发送信息，服务端返回信息的案例 使用DelimiterBasedFrameDecoder和StringDecoder解决粘包问题 分隔符使用"$_"

---
+++ netty-delimiter-decoder/src/main/java/com/airkisser/netty/client/EchoClient.java
@@ -0,0 +1,60 @@
+package com.airkisser.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by AIR on 2016/8/2.
+ */
+public class EchoClient {
+
+    public void connect(String host, int port) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                            socketChannel.pipeline()
+                                    .addLast(new LoggingHandler(LogLevel.INFO))
+                                    .addLast(new DelimiterBasedFrameDecoder(1024, delimiter))
+                                    .addLast(new StringDecoder())
+                                    .addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String host = "127.0.0.1";
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new EchoClient().connect(host, port);
+    }
+
+}
